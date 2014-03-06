6cd661340374ef48846e825b1e993a9f43cd1896
Ken Tominaga <ken.for.develop@gmail.com>
2014-03-06 19:21:49 +0900

    Make methods in InitialViewViewController.m

--- TrickCover/InitialViewController.m
+++ TrickCover/InitialViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "InitialViewController.h"
+#import "MenuViewController.h"
 
 @interface InitialViewController ()
 
@@ -33,6 +34,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// 横向きのスプラッシュ画面が作れないため、便宜的に縦向きのViewから横向きのViewに飛ばす
+- (void)viewDidAppear:(BOOL)animated
+{
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    MenuViewController *controller = [storyboard instantiateViewControllerWithIdentifier:@"Menu"];
+    controller.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
+    [self presentViewController:controller animated:YES completion:nil];
+    
+    NSLog(@"%@", @"goMenu");
+}
+
+// 回転処理が存在するかどうかを返す（回転しない）
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+// 回転する方向を指定（縦）
+- (NSUInteger)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
 }
 
 @end

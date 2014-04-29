a49d23f3972944670b088ecddc961a66a1aa2aa5
Ken Tominaga <ken.for.develop@gmail.com>
2014-04-30 02:28:25 +0900

    Just commit

--- TrickCover/controllers/MenuViewController.m
+++ TrickCover/controllers/MenuViewController.m
@@ -15,15 +15,16 @@
     int number;
     BOOL isAnimating;
     
-    IBOutlet UIImageView *cameraImage;
-    IBOutlet UIImageView *smallGear;
-    IBOutlet UIImageView *largeGear;
-    IBOutlet UIImageView *fbIcon;
-    IBOutlet UIImageView *photoLibrary;
+    UIImageView *cameraImage;
+    UIImageView *smallGear;
+    UIImageView *largeGear;
+    UIImageView *fbIcon;
+    UIImageView *photoLibrary;
     
     IBOutlet UIButton *cameraButton;
     IBOutlet UIButton *aboutButton;
-    IBOutlet UIButton *photoButton;
+    IBOutlet UIButton *albumButton;
+    IBOutlet UIButton *fbButton;
 }
 
 @end
@@ -45,7 +46,13 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    isAnimating = NO;
+    
     [self displayViews];
+    
+    [self rotateGears];
+    
+    [self moveCameraImage];
     
 }
 
@@ -55,17 +62,88 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewWillAppear:(BOOL)animated
+{
+    isAnimating = NO;
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    isAnimating = NO;
+}
+
 #pragma mark - Private method
 
 - (void)displayViews
 {
+    cameraImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"CameraButton.png"]];
+    cameraImage.frame = CGRectMake(345, 195, 100, 80);
+    cameraImage.contentMode = UIViewContentModeScaleAspectFit;
     cameraImage.transform = CGAffineTransformMakeRotation(22 * M_PI / 180);
+    [self.view addSubview:cameraImage];
     
+    smallGear = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"gear.png"]];
+    smallGear.frame = CGRectMake(140, 170, 30, 30);
+    smallGear.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:smallGear];
+    
+    largeGear = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"gear.png"]];
+    largeGear.frame = CGRectMake(165, 145, 50, 50);
+    largeGear.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:largeGear];
+    
+    photoLibrary = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"PhotoLibrary.png"]];
+    photoLibrary.frame = CGRectMake(236, 175, 70, 70);
+    photoLibrary.contentMode = UIViewContentModeScaleAspectFit;
+    [self.view addSubview:photoLibrary];
+    
+    fbIcon = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"facebook.png"]];
+    fbIcon.frame = CGRectMake(62, 92, 40, 100);
+    fbIcon.contentMode = UIViewContentModeScaleAspectFit;
     fbIcon.transform = CGAffineTransformMakeRotation(22 * M_PI / 180);
+    [self.view addSubview:fbIcon];
+    
 }
 
-- (void)rotateGears{
-    
+- (void)rotateGears
+{
+    if (!isAnimating) {
+        [UIView animateWithDuration:10.0f delay:0.0f options:UIViewAnimationOptionRepeat animations:^{
+            
+            smallGear.transform = CGAffineTransformRotate(smallGear.transform, 120 * M_PI / 180);
+            largeGear.transform = CGAffineTransformRotate(largeGear.transform, -120 * M_PI / 180);
+            
+        }completion:^(BOOL finished){
+            
+        }];
+    }
+}
+
+- (void)moveCameraImage
+{
+    if (!isAnimating){
+        
+        [UIView setAnimationDelegate:self];
+        [UIView animateWithDuration:1.0f
+                         animations:^{
+                             if (number % 2 == 0) {
+                                 cameraImage.transform = CGAffineTransformMakeRotation(16 * M_PI / 180);
+                             } else {
+                                 cameraImage.transform = CGAffineTransformMakeRotation(24 * M_PI / 180);
+                             }
+                         }completion:^(BOOL finished){ // アニメーション終了時の設定
+                             number = 1 - (number % 2);
+                             [self moveCameraImage];
+                             NSLog(@"camera = %d", number);
+                             NSLog(@"isAnimating = %d", isAnimating);
+                         }];
+    }
+
+}
+
+// アニメーションの終了時にコールされる。
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    isAnimating = NO; // ここでフラグを落とす。
 }
 
 #pragma mark - Orientation
